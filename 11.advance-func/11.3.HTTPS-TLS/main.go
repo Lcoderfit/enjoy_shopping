90a214fe2c53b3eea607eb1615023e1c3193b7d4
lcoderfit <1297611505@qq.com>
2021-09-14 23:43:22 +0800

    feature:添加HTTP/HTTPS

--- 11.advance-func/11.3.HTTPS-TLS/main.go
+++ 11.advance-func/11.3.HTTPS-TLS/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"github.com/gogf/gf/frame/g"
+	"github.com/gogf/gf/net/ghttp"
+)
 
+func main() {
+	s := ghttp.GetServer()
+	s.BindHandler("/", func(r *ghttp.Request) {
+		r.Response.Writeln("可以同时通过HTTPS和HTTP访问")
+	})
+	s.EnableHTTPS("/server.crt", "/server.key")
+	s.SetPort(80)
+	s.SetHTTPSPort(443)
+	s.Start()
+
+	g.Wait()
 }
+
