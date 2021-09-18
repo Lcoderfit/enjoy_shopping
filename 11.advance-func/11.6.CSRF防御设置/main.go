2988606f07f9c7f59a072f6fea9581a9978d2b74
lcoderfit <1297611505@qq.com>
2021-09-18 17:30:52 +0800

    feature:添加CSRF

--- 11.advance-func/11.6.CSRF防御设置/main.go
+++ 11.advance-func/11.6.CSRF防御设置/main.go
@@ -1,6 +1,43 @@
 package main
 
+import (
+	"github.com/gogf/csrf"
+	"github.com/gogf/gf/frame/g"
+	"github.com/gogf/gf/net/ghttp"
+	"net/http"
+	"time"
+)
 
 func main() {
-
+	s := g.Server()
+	s.Group("/api.v2", func(group *ghttp.RouterGroup) {
+		group.Middleware(csrf.NewWithCfg(csrf.Config{
+			TokenLength:     32,
+			TokenRequestKey: "X-Token",
+			ExpireTime:      time.Hour * 24,
+			Cookie: &http.Cookie{
+				Name: "_csrf", // token name in cookie
+			},
+		}))
+		group.ALL("/csrf", func(r *ghttp.Request) {
+			r.Response.Writeln(r.Method + ":" + r.RequestURI)
+		})
+	})
 }
+
+/*
+s := g.Server()
+s.Group("/api.v2", func(group *ghttp.RouterGroup) {
+	group.Middleware(csrf.NewWithCfg(csrf.Config{
+		Cookie: &http.Cookie{
+			Name: "_csrf",// token name in cookie
+		},
+		ExpireTime:      time.Hour * 24,
+		TokenLength:     32,
+		TokenRequestKey: "X-Token",// use this key to read token in request param
+	}))
+	group.ALL("/csrf", func(r *ghttp.Request) {
+		r.Response.Writeln(r.Method + ": " + r.RequestURI)
+	})
+})
+*/
