603642ba98de2fe3924c367ad4eececd4e643e53
Lcoderfit <1297611505@qq.com>
2021-10-01 13:35:10 +0800

    feature:添加tcp异步全双工通信示例

---
+++ 2.network/1.tcp/2.连接对象-通信开发进阶/funcs/funcs.go
@@ -0,0 +1,39 @@
+package funcs
+
+import (
+	"encoding/json"
+	"github.com/gogf/gf/errors/gerror"
+	"github.com/gogf/gf/net/gtcp"
+	"learn-gf/2.network/1.tcp/2.连接对象-通信开发进阶/types"
+)
+
+// 设置data参数为不定参的作用：可以设置当不传入data参数时的默认参数
+func SendPkg(conn *gtcp.Conn, act string, data ...string) error {
+	s := ""
+	if len(data) > 0 {
+		s = data[0]
+	}
+
+	msg, err := json.Marshal(types.Msg{
+		Act:  act,
+		Data: s,
+	})
+	if err != nil {
+		panic(err)
+	}
+	return conn.SendPkg(msg)
+}
+
+func RecvPkg(conn *gtcp.Conn) (msg *types.Msg, err error) {
+	data, err := conn.RecvPkg()
+	if err != nil {
+		return nil, err
+	} else {
+		msg = &types.Msg{}
+		err = json.Unmarshal(data, msg)
+		if err != nil {
+			return nil, gerror.Newf("invalid package structure:%s", err.Error())
+		}
+		return msg, err
+	}
+}
---
+++ 2.network/1.tcp/2.连接对象-通信开发进阶/server/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"github.com/gogf/gf/net/gtcp"
+	"github.com/gogf/gf/os/glog"
+	"github.com/gogf/gf/os/gtimer"
+	"learn-gf/2.network/1.tcp/2.连接对象-通信开发进阶/funcs"
+	"learn-gf/2.network/1.tcp/2.连接对象-通信开发进阶/types"
+	"time"
+)
+
+/*
+1.gtimer.SetTimeout(delay, job)
+	经过delay时间后执行一次job函数
+
+2.gtimer.SetInterval(interval, job)
+	没经过interval时间后执行一次job
+
+3.异步全双工通信
+*/
+
+func main() {
+	gtcp.NewServer("127.0.0.1:8300", func(conn *gtcp.Conn) {
+		defer conn.Close()
+		// 测试消息，10s后让客户端主动退出
+		// SetTimeout：传入两个参数，第一个是超时时间，第二个是func(){}函数，作用是经过超时时间后运行一次函数
+		gtimer.SetTimeout(10*time.Second, func() {
+			funcs.SendPkg(conn, "doexit")
+		})
+		for {
+			msg, err := funcs.RecvPkg(conn)
+			if err != nil {
+				if err.Error() == "EOF" {
+					glog.Println("client closed")
+				}
+				break
+			}
+			switch msg.Act {
+			case "hello":
+				onClientHello(conn, msg)
+			case "heartbeat":
+				onClientHeartBeat(conn, msg)
+			default:
+				glog.Errorf("invalid message: %v", msg)
+				break
+			}
+		}
+	})
+}
+
+func onClientHello(conn *gtcp.Conn, msg *types.Msg) {
+	glog.Printf("hello message from [%s]: %s", conn.RemoteAddr().String(), msg.Data)
+	funcs.SendPkg(conn, msg.Act, "Nice to meet you")
+}
+
+func onClientHeartBeat(conn *gtcp.Conn, msg *types.Msg) {
+	glog.Printf("heartbeat from [%s]", conn.RemoteAddr().String())
+}
